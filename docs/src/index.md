e913f474afcf180f3d7be2fa6da4329b373e8d9e
Anshul Singhvi <asinghvi17@simons-rock.edu>
2019-07-12 16:33:55 +0530

    Make a function and export

--- docs/src/index.md
+++ docs/src/index.md
@@ -1,8 +1,18 @@
-# ForTheBadge.jl
+# ForTheBadge
 
 ```@index
 ```
 
-```@autodocs
-Modules = [ForTheBadge]
+This is a very simple package, which will generate a badge in the style of (ForTheBadge)[https://forthebadge.com/].
+
+```@docs
+badge
+```
+
+Note that output SVGs have text drawn as a path, not as text; this is to ensure maximum portability.
+
+# Examples
+
+```@jldoctest
+badge("USING", "MAKIE")
 ```
--- src/ForTheBadge.jl
+++ src/ForTheBadge.jl
@@ -1,99 +1,78 @@
 module ForTheBadge
 
-using Colors
-using Luxor
+    using Colors
+    using Luxor
 
-rtext = "60% OF THE TIME"
-ltext = "WORKS EVERY TIME"
+    """
+        badge(
+            labels::NTuple{N, AbstractString};
+            fonts::NTuple{N, AbstractString} = ("Roboto Medium", "Montserrat ExtraBold")
+            bgcolours::NTuple{N, T} = (colorant"#a7bfc1", colorant"#5593c8"),
+            textcolours::NTuple{N, T} = (colorant"white", colorant"white"),
+            padding = 13,
+            h = 50,
+            ext = :pdf,
+            filename = join(labels, "_") * ".$ext",
+            textsize = 20,
+            showbadg = true
+        )
 
-rcolor = colorant"blue"
-lcolor = colorant"red"
+        badge(labels...; kwargs...)
 
-# Drawing(1000, 1000, :pdf, "lol.pdf")
-@pdf begin
 
-    origin(100, 100)
+    Provided labels, font sizes and colours of the appropriate type,
+    this function will create a badge in the style of (ForTheBadge)[https://forthebadge.com/].
 
-    fontsize(20)
+    Input must be in the form of tuples.
+    """
+    function badge(
+            labels::NTuple{N, AbstractString};
+            fonts::NTuple{N, AbstractString} = ("Roboto Medium", "Montserrat ExtraBold"),
+            bgcolours::NTuple{N, T} = (colorant"#a7bfc1", colorant"#5593c8"),
+            textcolours::NTuple{N, T} = (colorant"white", colorant"white"),
+            padding = 13,
+            h = 50,
+            ext = :pdf,
+            filename = join(labels, "_") * ".$ext",
+            textsize = 20,
+            showbadge = true
+        ) where N where T
 
-    local lbb
+        Drawing(800, h, :png) # initialize a dummy, in-memory drawing to get text extents
 
-    @layer begin
+        fontsize(textsize) # set the font size
 
-        rbb = BoundingBox(rtext) + 13 * 2
+        rawextents = [begin fontface(fonts[i]); ceil(textextents(labels[i])[3]) + padding * 2 end for i in eachindex(labels)]
+        finish()
+        pushfirst!(rawextents, 0.0)
 
-        sethue(colorant"green")
+        extents = [sum(rawextents[1:i]) for i in eachindex(rawextents)]
 
-        poly(rbb, :fill, close=true)
+        xlength = sum(rawextents)
 
-        lbb = (BoundingBox(ltext) + 13 * 2) + (rbb.corner2.x + 13, 0)
+        midpoints = ((rawextents[i]) / 2 + ((i == zero(typeof(i))) ? 0 : sum(rawextents[1:i-1])) for i in eachindex(extents)) |> collect
 
-        sethue(colorant"blue")
+        Drawing(xlength, h, filename)
 
-        poly(lbb, :fill, close=true)
+        for (k, p) in enumerate(midpoints[2:end])
+            sethue(bgcolours[k])
+            box(Point(extents[k], 0), Point(extents[k+1], 50), :fill)
+            sethue(textcolours[k])
+            fontface(fonts[k])
+            fontsize(textsize)
+            textoutlines(labels[k], Point(p, 25), halign=:center, valign=:middle)
+            fillpreserve()
+        end
 
+        finish()
+
+        showbadge && preview()
+
+        filename
     end
 
-    sethue(colorant"white")
+    badge(labels...; kwargs...) = badge(Tuple(labels); kwargs...)
 
-    fontface("Roboto Medium")
-
-    text(rtext)
-
-    fontface("Montserrat ExtraBold")
-
-    text(ltext, Point(lbb.corner1.x + 13, 0); valign = :centre)
+    export badge
 
 end
-
-end # module
-
-
-using Luxor
-@pdf begin
-    t = Tiler(200, 50, 1, 2, margin=0)
-    fontsize(20)
-    labels=["TEST IT", "SHIP IT"]
-    for (k, p) in enumerate(t)
-        randomhue()
-        box(first(p), 100, 50, :fill)
-        sethue("white")
-        text(labels[k], first(p), halign=:center, valign=:middle)
-    end
-end 200 50 "/tmp/badge"
-
-local xlength
-using Pkg; Pkg.activate(".")
-# @pdf begin
-using Luxor, Colors
-Drawing(800, 50)
-fontsize(20)
-labels=["BUILT WITH", "GRAV"]
-fonts = ["Roboto Medium", "Montserrat ExtraBold"]
-bgcolours = [colorant"orange", colorant"dark orange"]
-textcolours = [colorant"white", colorant"white"]
-
-rawextents = [begin fontface(fonts[i]); ceil(textextents(labels[i])[3]) + 26 end for i in eachindex(labels)]
-finish()
-pushfirst!(rawextents, 0.0)
-
-extents = [sum(rawextents[1:i]) for i in eachindex(rawextents)]
-
-xlength = sum(rawextents)
-
-
-midpoints = ((rawextents[i]) / 2 + ((i == zero(typeof(i))) ? 0 : sum(rawextents[1:i-1])) for i in eachindex(extents)) |> collect
-
-Drawing(xlength, 50, "label.svg")
-
-for (k, p) in enumerate(midpoints[2:end])
-    sethue(bgcolours[k])
-    box(Point(extents[k], 0), Point(extents[k+1], 50), :fill)
-    sethue(textcolours[k])
-    fontface(fonts[k])
-    fontsize(20)
-    text(labels[k], Point(p, 25), halign=:center, valign=:middle)
-end
-finish()
-preview()
-# end xlength 50 "/tmp/badge"
