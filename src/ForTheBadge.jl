fdfdffd691dbb747d87dd7acb32c89af3089ad4b
Anshul Singhvi <asinghvi17@simons-rock.edu>
2019-07-13 11:23:20 +0530

    Better docstring and assert

--- src/ForTheBadge.jl
+++ src/ForTheBadge.jl
@@ -5,16 +5,17 @@ module ForTheBadge
 
     """
         badge(
-            labels::NTuple{N, AbstractString};
-            fonts::NTuple{N, AbstractString} = ("Roboto Medium", "Montserrat ExtraBold")
-            bgcolours::NTuple{N, T} = (colorant"#a7bfc1", colorant"#5593c8"),
-            textcolours::NTuple{N, T} = (colorant"white", colorant"white"),
+            labels; # must be the same length as kwargs, so if you
+            fonts = ("Roboto Medium", "Montserrat ExtraBold")
+            bgcolours = (colorant"#a7bfc1", colorant"#5593c8"),
+            textcolours = (colorant"white", colorant"white"),
             padding = 13,
             h = 50,
             ext = :pdf,
             filename = join(labels, "_") * ".\$ext",
             textsize = 20,
-            showbadg = true
+            texttype = :path,
+            showbadge = true
         )
 
         badge(labels...; kwargs...)
@@ -24,6 +25,26 @@ module ForTheBadge
     this function will create a badge in the style of (ForTheBadge)[https://forthebadge.com/].
 
     Input must be in the form of tuples.
+
+    ## Keyword arguments
+
+        * `texttype` may be `:path` or `:text`.
+        * `ext` may be `:pdf`, `:png`, or `:svg`.
+        * `showbadge` controls whether the badge is opened
+          by the system viewer after it is drawn.
+
+    ## Examples:
+
+    ```julia
+    # To generate the logo:
+    badge(
+        "FOR", "THE", "BADGE", ".jl";
+        filename = "logo.svg",
+        textcolours = (colorant"white", colorant"white", colorant"white", colorant"white"),
+        bgcolours = (julia_green, julia_purple, julia_red, julia_blue),
+        fonts = ("Roboto Medium", "Roboto Medium", "Montserrat ExtraBold", "Roboto Medium")
+    )
+    ```
     """
     function badge(
             labels;
@@ -35,8 +56,11 @@ module ForTheBadge
             ext = :pdf,
             filename = join(labels, "_") * ".$ext",
             textsize = 20,
+            texttype = :path,
             showbadge = true
         ) where N where T
+
+        @assert length(labels) == length(fonts) == length(bgcolours) == length(textcolours) 
 
         Drawing(800, h, :png) # initialize a dummy, in-memory drawing to get text extents
 
@@ -74,5 +98,7 @@ module ForTheBadge
     badge(labels...; kwargs...) = badge(Tuple(labels); kwargs...)
 
     export badge
+
+    export Luxor.julia_blue, Luxor.julia_purple, Luxor.julia_green, Luxor.julia_red
 
 end
